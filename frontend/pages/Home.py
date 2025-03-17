8f450b2145ffac1ad576625a84619c77e50431b5
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-03-17 15:45:58 -0400

    add (pages Home): se agrego metodo para abrir archivos .java, poder leerlos y escribir el codigo en el textArea

--- frontend/pages/Home.py
+++ frontend/pages/Home.py
@@ -4,10 +4,14 @@ from Components.textArea import TextArea
 class Home_page():
     def __init__(self, page):
         self.page = page
+        self.text_field = None
+        self.file_picker = ft.FilePicker(on_result=self.read_file)
+        self.page.add(self.file_picker)  # Agregar el FilePicker a la página
 
     def buil_page(self):
 
         component = TextArea(self.page)
+        self.text_field = component.textField
 
         return ft.Column(
                 controls=[
@@ -18,7 +22,7 @@ class Home_page():
                     ft.Container(content=component.textArea_component(), padding=ft.padding.only(bottom=20)),
                     ft.Row(
                         controls=[
-                            ft.ElevatedButton("Cargar Archivo", bgcolor="#64A6F5", color="white", width=200, height=50),
+                            ft.ElevatedButton("Cargar Archivo", bgcolor="#64A6F5", color="white", width=200, height=50, on_click=self.load_file),
                             ft.ElevatedButton("Analizar Código", bgcolor="#64A6F5", color="white", width=200, height=50)
                         ],
                         alignment=ft.MainAxisAlignment.CENTER,
@@ -28,4 +32,21 @@ class Home_page():
                 alignment=ft.MainAxisAlignment.CENTER,
                 horizontal_alignment=ft.CrossAxisAlignment.CENTER,
             )
+    
+    def load_file(self, e):
+        self.file_picker.pick_files(allow_multiple=False)
+
+    def read_file(self, e):
+        if e.files:
+            file_path = e.files[0].path
+            if file_path.endswith(".java"):  # Verificar si el archivo es .java
+                with open(file_path, "r") as file:
+                    code = file.read()
+                    print(code)
+                    self.text_field.value = code
+                    print("Texto asignado al TextField")
+                    self.page.update()
+                    print("Página actualizada")
+            else:
+                print("Por favor, seleccione un archivo .java")
 

e2f3107c9fbf200c0dcd16930f3188899cd146e0
Sperrotta10 <santiagoperrotta10@gmail.com>
2025-04-15 16:33:38 -0400

    add (gui.py): se agrego la logica para navegar a la pagina semantica y cod_final

--- frontend/gui.py
+++ frontend/gui.py
@@ -2,6 +2,8 @@ import flet as ft
 from pages.Home import Home_page
 from pages.Lexico import Lexico_page
 from pages.sintactico import Sintactico_page
+from pages.semantico import Semantico_page
+from pages.cod_final import CodFinal_page
 from Components.header import Header
 
 def main(page: ft.Page):
@@ -16,15 +18,16 @@ def main(page: ft.Page):
     page.file_content = ""  # Inicializar el contenido del archivo
     page.tokens = None # Inicializar lista de tokens
 
-    # Instancia única de Home_page, Lexico_page, Sintactico_page
+    # Instancia única de cada página
     page.page_h = None
     page.page_l = None
     page.page_s = None
+    page.page_sem = None
+    page.page_cf = None
 
     # Función para construir contenido específico de cada página
     def build_page_content(page_name, page):
         if page_name == "home":
-
             if not page.page_h:
                 page.page_h = Home_page(page, page.file_content)
 
@@ -33,7 +36,6 @@ def main(page: ft.Page):
             return content
 
         elif page_name == "lexico":
-            
             if not page.page_l:
                 page.page_l = Lexico_page(page, page.file_content)
             
@@ -47,20 +49,30 @@ def main(page: ft.Page):
                 page.page_l = Lexico_page(page, page.file_content)  # Asegurar que Lexico_page está inicializado
 
             page.page_l.update_code(page.file_content)  # Asegurar que los tokens están actualizados
-            page.page_s =  Sintactico_page(page, page.page_l)            
+            page.page_s = Sintactico_page(page, page.page_l)            
 
             content = page.page_s.buil_page()
             return content
 
-
         elif page_name == "semantico":
-            return ft.Text("Página de Análisis Semántico")
-        elif page_name == "cod_medio":
-            return ft.Text("Página de Código Intermedio")
-        elif page_name == "optimizacion":
-            return ft.Text("Página de Optimización")
+            if not page.page_s:
+                # Si no hay análisis sintáctico, redirigir a esa página
+                page.current_page = "sintactico"
+                return build_page_content("sintactico", page)
+            
+            page.page_sem = Semantico_page(page, page.page_s)
+            content = page.page_sem.buil_page()
+            return content
+
         elif page_name == "cod_final":
-            return ft.Text("Página de Código Final")
+            if not page.page_sem:
+                # Si no hay análisis semántico, redirigir a esa página
+                page.current_page = "semantico"
+                return build_page_content("semantico", page)
+            
+            page.page_cf = CodFinal_page(page, page.page_sem)
+            content = page.page_cf.buil_page()
+            return content
 
     # Función para navegar entre páginas
     def navigate_to(page_name):
@@ -73,6 +85,9 @@ def main(page: ft.Page):
             build_page_content(page_name, page)
         )
         page.update()
+    
+    # Agregar método de navegación al objeto page para que sea accesible desde otras clases
+    page.navigate_to = navigate_to
 
     # Función para actualizar el diseño al cambiar el tamaño de la ventana
     def update_layout(e):
